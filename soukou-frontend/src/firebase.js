d1884dd193df0c84d607aa5476104940e9daea72
Tatsuro Baba <harakirisoul@gmail.com>
2019-09-02 20:46:44 +0900

    wip

---
+++ soukou-frontend/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
